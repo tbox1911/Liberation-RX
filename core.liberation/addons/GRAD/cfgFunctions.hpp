7012195eb6553f9b7a0bd2a425e9d5a34d6cbf91
tbox <tbox1911@gmail.com>
2020-04-13 11:41:27 +0200

    cosm

--- core.liberation/addons/GRAD/cfgFunctions.hpp
+++ core.liberation/addons/GRAD/cfgFunctions.hpp
@@ -1,14 +1,10 @@
 class GRAD_fireworks {
-
     class client {
-            file = addons\GRAD\functions\client;
-
+            file = "addons\GRAD\functions\client";
             class spawnFireworks {};
     };
-
     class server {
-            file = addons\GRAD\functions\server;
-
+            file = "addons\GRAD\functions\server";
             class prepareFireworks {};
     };
 };
