1240c50b15a06f863d3111d57d57de22a107d434
tbox <tbox1911@gmail.com>
2022-01-20 10:38:11 +0100

    add SoG

--- core.liberation/ui/mission_params.hpp
+++ core.liberation/ui/mission_params.hpp
@@ -98,7 +98,7 @@ class Params
 	};
 	class ModPresetWest {
 		title = "MOD Preset - Friendly";
-		values[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+		values[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 };
 		texts[] = {
 					"A3 NATO",
 					"A3 CSAT",
@@ -115,13 +115,15 @@ class Params
 					"GM WEST",
 					"GM WEST Winter",
 					"OPTRE West",
-					"WS UNA"
+					"WS UNA",
+					"SoG USA",
+					"SoG VIETCONG"
 				};
 		default = 0;
 	};
 	class ModPresetEast {
 		title = "MOD Preset - Enemy";
-		values[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17 };
+		values[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19 };
 		texts[] = {
 					"A3 CSAT",
 					"A3 NATO",
@@ -140,7 +142,9 @@ class Params
 					"GM EAST",
 					"GM EAST Winter",
 					"OPTRE East",
-					"WS FIA"
+					"WS FIA",
+					"SoG USA",
+					"SoG VIETCONG"
 				};
 		default = 0;
 	};
