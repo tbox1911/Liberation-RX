e85b162af35307dae500d50a093ee75f75aa6667
Sonic47 <bourget.stephan@hotmail.ch>
2022-03-12 20:18:37 +0100

    Update build.bat
    damit unsere Karten gepackt werden

--- build/build.bat
+++ build/build.bat
@@ -9,11 +9,11 @@ echo GRLIB_build_date = "%DATE%"; >> %GRLIB_file%
 echo GRLIB_build_time = "%TIME:~0,8%"; >> %GRLIB_file%
 echo diag_log format ["Build date: %%1", GRLIB_build_date]; >> %GRLIB_file%
 
-for /f %%i in ('dir /B /A:D ..\maps\liberation_RX*') do (
+for /f %%i in ('dir /B /A:D ..\maps-MilSimUnited\liberation_RX*') do (
 	echo.
 	echo Building PBO for map %%i 
 	xcopy /Q /E /Y ..\core.liberation .\%%i\
-	xcopy /Q /E /Y ..\maps\%%i .\%%i\
+	xcopy /Q /E /Y ..\maps-MilSimUnited\%%i .\%%i\
 	if exist .\custom\ xcopy /Q /E /Y .\custom .\%%i\
 	bin\PBOConsole.exe -pack %%i .\%%i.pbo  > nul 2>&1
 	rmdir /S /Q %%i
