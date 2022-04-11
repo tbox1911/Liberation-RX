bdb79c3633b308841930292f5b522b2cc02149c8
tbox <tbox1911@gmail.com>
2022-04-11 12:44:09 +0200

    sell shop dialog

--- core.liberation/addons/SELL/sell_shop.hpp
+++ core.liberation/addons/SELL/sell_shop.hpp
@@ -1,15 +1,15 @@
-class Traders_Shop {
-  	idd = 2304;
-	name = "Traders_Shop";
+class Sell_Shop {
+  	idd = 2305;
+	name = "Sell_Shop";
 	movingEnable = false;
 	enableSimulation = true;
 
 	class controlsBackground {
 		class OuterBG1: StdBG {
 			colorBackground[] = COLOR_BROWN;
-			x = 0.19175 * safezoneW + safezoneX;
+			x = 0.1917 * safezoneW + safezoneX;
 			y = 0.1324 * safezoneH + safezoneY;
-			w = 0.33 * safezoneW;
+			w = 0.1651 * safezoneW;
 			h = 0.62 * safezoneH;
 		};
 		class OuterBG_F1: OuterBG1 {
@@ -26,24 +26,14 @@ class Traders_Shop {
 			style = ST_FRAME;
 		};
 		
-		class InnerBG2: OuterBG1 {
-			colorBackground[] = COLOR_GREEN;
-			x = 0.3630 * safezoneW + safezoneX;
-			y = 0.1916 * safezoneH + safezoneY;
-			w = 0.1515 * safezoneW;
-			h = 0.512 * safezoneH;
-		};
-		class InnerBG_F2: InnerBG2 {
-			style = ST_FRAME;
-		};
 	};
 
 	class controls {
 		class Header: StdHeader {
-			text = "$STR_SHOP_NAME";
+			text = "$STR_SELL_NAME";
 			x = 0.198875 * safezoneW + safezoneX;
 			y = 0.1414 * safezoneH + safezoneY;
-			w = 0.314531 * safezoneW;
+			w = 0.1515 * safezoneW;
 			h = 0.044 * safezoneH;
 		};
 
@@ -58,22 +48,7 @@ class Traders_Shop {
 			colorPictureDisabled[] = {0.4,0.4,0.4,1};
 			x = 0.2027 * safezoneW + safezoneX;
 			y = 0.2074 * safezoneH + safezoneY;
-			w = 0.1444 * safezoneW;
-			h = 0.484 * safezoneH;
-		};
-
-		class VehList2: StdListNBox {
-			idc = 111;
-			columns[] = {0,0.78};
-			onLBSelChanged = "";
-			shadow = 2;
-			rowHeight = "1.25 * 0.018 * safezoneH";
-			colorPicture[] = {1,1,1,1};
-			colorPictureSelected[] = {0,1,0,1};
-			colorPictureDisabled[] = {0.4,0.4,0.4,1};
-			x = 0.3679 * safezoneW + safezoneX;
-			y = 0.2074 * safezoneH + safezoneY;
-			w = 0.1444 * safezoneW;
+			w = 0.1515 * safezoneW;
 			h = 0.484 * safezoneH;
 		};
 
@@ -81,7 +56,7 @@ class Traders_Shop {
 			idc = -1;
 			action = "closeDialog 0";
 			text = "X";
-			x = 0.4950 * safezoneW + safezoneX;
+			x = 0.3338 * safezoneW + safezoneX;
 			y = 0.1414 * safezoneH + safezoneY;
 			w = 0.015 * safezoneW;
 			h = 0.02 * safezoneH;
@@ -91,20 +66,12 @@ class Traders_Shop {
 		class SellButton : StdButton {
 			idc = 120;
 			text = $STR_SHOP_ACTION1;
-			action = "shop_action = 1";
-			x = 0.23 * safezoneW + safezoneX;
-			y = 0.71 * safezoneH + safezoneY;
+			action = "sell_action = 1";
+			x = 0.2300 * safezoneW + safezoneX;
+			y = 0.7100 * safezoneH + safezoneY;
 			w = 0.09 * safezoneW;
 			h = 0.035 * safezoneH;
 		};
-		class BuyButton : StdButton {
-			idc = 121;
-			text = "$STR_SHOP_ACTION2";
-			action = "shop_action = 2";
-			x = 0.40 * safezoneW + safezoneX;
-			y = 0.71 * safezoneH + safezoneY;
-			w = 0.09 * safezoneW;
-			h = 0.035 * safezoneH;
-		};
+
 	};
 };
