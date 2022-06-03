f89d1e417794a0b1b1aa0802d50da4e394eb3620
Mantanismus <44084142+Mantanismus@users.noreply.github.com>
2022-06-03 18:33:21 +0200

    removed build info

--- build/build.bat
+++ build/build.bat
@@ -3,12 +3,6 @@
 echo - Liberation_RX PBO build script -
 del /f *.pbo  > nul 2>&1
 
-set GRLIB_file="..\core.liberation\build_info.sqf"
-echo //Liberation_RX was build on : > %GRLIB_file%
-echo GRLIB_build_date = "%DATE%"; >> %GRLIB_file%
-echo GRLIB_build_time = "%TIME:~0,8%"; >> %GRLIB_file%
-echo diag_log format ["Build date: %%1", GRLIB_build_date]; >> %GRLIB_file%
-
 for /f %%i in ('dir /B /A:D ..\maps-MilSimUnited\liberation_RX*') do (
 	echo.
 	echo Building PBO for map %%i 
