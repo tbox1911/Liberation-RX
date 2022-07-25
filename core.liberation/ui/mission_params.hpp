e51535af555e530c3832fa849a2dbb0f15dca047
fingerratte <fingerratte@users.noreply.github.com>
2022-07-25 18:50:23 +0200

    Patrols on

--- core.liberation/ui/mission_params.hpp
+++ core.liberation/ui/mission_params.hpp
@@ -208,7 +208,7 @@ class Params
 		title = $STR_PARAM_PATROL;
 		values[] = {1,0};
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 0;
+		default = 1;
 	};
 	class TeamkillPenalty{
 		title = $STR_PARAM_TEAMKILL_PENALTY;
