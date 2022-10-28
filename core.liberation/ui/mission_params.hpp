96c05ce19f37d1e3410439eee887e9464c5d2b22
Huber-Sepp <80065164+Huber-Sepp@users.noreply.github.com>
2022-10-29 01:13:12 +0200

    little less aggressive

--- core.liberation/ui/mission_params.hpp
+++ core.liberation/ui/mission_params.hpp
@@ -118,7 +118,7 @@ class Params
 		title = $STR_AGGRESSIVITY_PARAM;
 		values[] = {0.25,0.5,1,2,3};
 		texts[] = {$STR_AGGRESSIVITY_PARAM0, $STR_AGGRESSIVITY_PARAM1,$STR_AGGRESSIVITY_PARAM2,$STR_AGGRESSIVITY_PARAM3,$STR_AGGRESSIVITY_PARAM4};
-		default = 2;
+		default = 1;
 	};
 	class AdaptToPlayercount{
 		title = $STR_PARAM_ADAPT_TO_PLAYERCOUNT;
