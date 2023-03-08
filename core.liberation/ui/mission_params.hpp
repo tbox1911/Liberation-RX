782bc68f9ea3f3f2a295bb49d5aaaf71b62ef6df
Huber-Sepp <80065164+Huber-Sepp@users.noreply.github.com>
2023-03-08 22:46:42 +0100

    deployment cinematic

--- core.liberation/ui/mission_params.hpp
+++ core.liberation/ui/mission_params.hpp
@@ -304,7 +304,7 @@ class Params
 		title = $STR_PARAMS_DEPLOYMENTCAMERA;
 		values[] = { 1, 0 };
 		texts[] = { $STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED };
-		default = 0;
+		default = 1;
 	};
 /*	class FirstFob {
 		title = $STR_PARAMS_FIRSTFOB;
