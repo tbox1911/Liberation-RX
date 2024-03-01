05619cf6646d7c4711097c26361927943d9b3d1d
tbox1911 <tbox1911@gmail.com>
2024-03-01 18:03:30 +0100

    dialog def

--- core.liberation/addons/FOB/def_sector.hpp
+++ core.liberation/addons/FOB/def_sector.hpp
@@ -9,7 +9,7 @@ class Sector_Defense {
 			colorBackground[] = COLOR_BROWN;
 			x = 0.1917 * safezoneW + safezoneX;
 			y = 0.1324 * safezoneH + safezoneY;
-			w = 0.1651 * safezoneW;
+			w = 0.2476 * safezoneW;
 			h = 0.62 * safezoneH;
 		};
 		class OuterBG_F1: OuterBG1 {
@@ -20,7 +20,7 @@ class Sector_Defense {
 			x = 0.1985 * safezoneW + safezoneX;
 			y = 0.1916 * safezoneH + safezoneY;
 			w = 0.1515 * safezoneW;
-			h = 0.512 * safezoneH;
+			h = 0.55 * safezoneH;
 		};
 		class InnerBG_F1: InnerBG1 {
 			style = ST_FRAME;
@@ -29,16 +29,16 @@ class Sector_Defense {
 
 	class controls {
 		class Header: StdHeader {
-			text = "SECTOR LIST";
+			text = "SECTORS LIST";
 			x = 0.198875 * safezoneW + safezoneX;
 			y = 0.1414 * safezoneH + safezoneY;
-			w = 0.1515 * safezoneW;
+			w = 0.2340 * safezoneW;
 			h = 0.044 * safezoneH;
 		};
 
 		class VehList1 : StdListNBox {
 			idc = 110;
-			columns[] = {0,0.78};
+			columns[] = {0,0.75};
 			onLBSelChanged = "";
 			shadow = 2;
 			rowHeight = "1.25 * 0.018 * safezoneH";
@@ -47,30 +47,68 @@ class Sector_Defense {
 			colorPictureDisabled[] = {0.4,0.4,0.4,1};
 			x = 0.2027 * safezoneW + safezoneX;
 			y = 0.2074 * safezoneH + safezoneY;
-			w = 0.1515 * safezoneW;
-			h = 0.484 * safezoneH;
+			w = 0.1480 * safezoneW;
+			h = 0.53 * safezoneH;
 		};
 
 		class CancelButton : StdButton {
 			idc = -1;
 			action = "closeDialog 0";
 			text = "X";
-			x = 0.3338 * safezoneW + safezoneX;
+			x = 0.4163 * safezoneW + safezoneX;
 			y = 0.1414 * safezoneH + safezoneY;
 			w = 0.015 * safezoneW;
 			h = 0.02 * safezoneH;
 			default = true;
 		};
 
-		class SellButton : StdButton {
+		class Button0 : StdButton {
 			idc = 120;
-			text = "BUILD";
-			action = "build_action = 1";
-			x = 0.2300 * safezoneW + safezoneX;
-			y = 0.7100 * safezoneH + safezoneY;
-			w = 0.09 * safezoneW;
-			h = 0.035 * safezoneH;
+			text = "OFF";
+			action = "build_type = 0; build_action = 1";
+			x = 0.3572 * safezoneW + safezoneX;
+			y = 0.2074 * safezoneH + safezoneY;
+			w = 0.07 * safezoneW;
+			h = 0.03 * safezoneH;
 		};
 
+		class Button1 : StdButton {
+			idc = 121;
+			text = "Light";
+			action = "build_type = 1; build_action = 1";
+			x = 0.3572 * safezoneW + safezoneX;
+			y = 0.2474 * safezoneH + safezoneY;
+			w = 0.07 * safezoneW;
+			h = 0.03 * safezoneH;
+		};
+
+		class Button2 : StdButton {
+			idc = 122;
+			text = "Medium";
+			action = "build_type = 2; build_action = 1";
+			x = 0.3572 * safezoneW + safezoneX;
+			y = 0.2874 * safezoneH + safezoneY;
+			w = 0.07 * safezoneW;
+			h = 0.03 * safezoneH;
+		};
+
+		class Button3 : StdButton {
+			idc = 123;
+			text = "High";
+			action = "build_type = 3; build_action = 1";
+			x = 0.3572 * safezoneW + safezoneX;
+			y = 0.3274 * safezoneH + safezoneY;
+			w = 0.07 * safezoneW;
+			h = 0.03 * safezoneH;
+		};
+
+		class ButtonExit : StdButton {
+			text = "Exit";
+			action = "closeDialog 0";
+			x = 0.3572 * safezoneW + safezoneX;
+			y = 0.4474 * safezoneH + safezoneY;
+			w = 0.07 * safezoneW;
+			h = 0.03 * safezoneH;
+		};				
 	};
 };
